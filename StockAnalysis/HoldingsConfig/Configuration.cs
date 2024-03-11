0445848a0bbbfb3f1668faee1aaf124541431d78
Matej Vavrek <matvav13@gmail.com>
2024-03-11 16:55:15 +0100

    feat: load holdings from .json

---
+++ StockAnalysis/HoldingsConfig/Configuration.cs
@@ -0,0 +1,33 @@
+using System.Text.Json;
+using StockAnalysis.Download;
+
+namespace StockAnalysis.HoldingsConfig;
+
+public class Configuration
+{
+    public string ConfigurationPath { get; }
+
+    public Configuration(string configurationPath)
+    {
+        ConfigurationPath = configurationPath;
+    }
+
+    /// <summary>
+    /// Loads configuration from preset file path.
+    /// </summary>
+    /// <returns>Information about desired ETF holdings in an array. Can be empty if an issue arose during loading.</returns>
+    public async Task<HoldingInformation[]> LoadConfiguration()
+    {
+        try
+        {
+            await using var reader = File.OpenRead(ConfigurationPath);
+            var holding = await JsonSerializer.DeserializeAsync<Holdings>(reader);
+            return holding.HoldingInfo.ToArray();
+        }
+        //ToDo: Gotta catch 'em all!
+        catch (ArgumentNullException e)
+        {
+            return Array.Empty<HoldingInformation>();
+        }
+    }
+}
--- StockAnalysis/HoldingsConfig/HoldingInformation.cs
+++ StockAnalysis/HoldingsConfig/HoldingInformation.cs
@@ -1,5 +1,10 @@
 namespace StockAnalysis.Download;
 
+public struct Holdings
+{
+    public IEnumerable<HoldingInformation> HoldingInfo { get; set;  }
+}
+
 public struct HoldingInformation
 {
     public string Name { get; set; }
