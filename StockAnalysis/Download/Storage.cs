266e3b4de7e45b738c7d7f4b7d536013f7dff133
Matej Vavrek <matvav13@gmail.com>
2024-03-11 16:58:02 +0100

    feat: Documentation for the Storage class.

--- StockAnalysis/Download/Storage.cs
+++ StockAnalysis/Download/Storage.cs
@@ -2,12 +2,18 @@ namespace StockAnalysis.Download;
 
 public class Storage
 {
-
+    /// <summary>
+    /// Stores data within a stream into the filesystem, combining path and name to form a file.
+    /// </summary>
+    /// <param name="stream">A stream that will be completely stored into a file. This function DOES NOT close the stream.</param>
+    /// <param name="path">The path to where the stream contents should be stored.</param>
+    /// <param name="name">The desired name of the file that will be created.</param>
     public static async Task WriteToFileSystem(Stream stream, string path, string name)
     {
         var finalPath = Path.Combine(path, name);
         try
         {
+            stream.Seek(0, SeekOrigin.Begin);
             await using var fileStream = File.Create(finalPath);
             await stream.CopyToAsync(fileStream);
         }
