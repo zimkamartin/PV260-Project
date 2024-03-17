5ef8e8c2a71c0bfce2498d7d214021d1596aa423
Matej Vavrek <59949784+MatV404@users.noreply.github.com>
2024-03-17 09:04:51 +0100

    Api pull (#1)
    * feat: started work on downloading
    
    * feat: load holdings from .json
    
    * feat: Documentation for the Storage class.
    
    * feat: tests for the config loader
    
    * fix: Light changes on error handling, comments
    
    * feat: Added tests, removed DummyClass.cs
    
    * fix: refactored tests and removed unused using in GlobalUsings.cs
    
    * fix: fixed issue in multiple download, where only the first file would be downloaded.
    
    * fix: Download process documentation and exception handling
    
    * fix: Configuration documentation && Config load test improvement
    
    * fix: separated modules into namespaces
    
    * Polished up tests and created a more 'atomic' test for downloads. May need more testing, though.
    
    * fix: Removed the redundant UnitTest1.cs file.

---
+++ StockAnalysis/HoldingsConfig/HoldingInformation.cs
@@ -0,0 +1,24 @@
+namespace StockAnalysis.HoldingsConfig;
+
+public struct Holdings
+{
+    public IEnumerable<HoldingInformation> HoldingInfo { get; set;  }
+}
+
+/// <summary>
+/// A structure that contains information about individual ETF holdings.
+/// The name is used to later construct files for downloaded data, while the
+/// uri itself must be the endpoint for a GET request that can download data
+/// about the given ETF holding.
+/// </summary>
+public struct HoldingInformation
+{
+    public string Name { get; set; }
+    public string Uri { get; set; }
+
+    public HoldingInformation(string name, string uri)
+    {
+        Name = name;
+        Uri = uri;
+    }
+}
