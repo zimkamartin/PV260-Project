462e1d21d5df872873726b7bc6ef4d4939755e80
Martin Mojžiš <xmojzis1@fi.muni.cz>
2024-03-22 09:55:23 +0100

    tests wip

--- StockAnalysis.Tests/DiffTests/DiffStoreTests.cs
+++ StockAnalysis.Tests/DiffTests/DiffStoreTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework.Internal;
+﻿using System.Globalization;
+using NUnit.Framework.Internal;
 using StockAnalysis.Diff;
 
 namespace StockAnalysisTests.DiffTests;
@@ -18,13 +19,40 @@ public class DiffStoreTests
             _projectRoot = projectDirectory.Parent!.Parent!.FullName;
         }
     }
-
-    //creates file diff.csv
+    
     [Test]
-    public async Task StoreDiff_shouldReturnTrue()
+    public async Task StoreDiff_shouldReturnTrueFileExists()
     {
+        //act
         List<DiffData> data = DiffComputer.CreateDiff(Path.Combine(_projectRoot, "test.csv"));
         bool result = await DiffStore.StoreDiffToCsv(data, _projectRoot, "test_diff");
+        
+        //assert
         Assert.IsTrue(result);
+        var totalPath = Path.Join(_projectRoot, "test_diff.csv");
+        Assert.That(File.Exists(totalPath), Is.True);
+        
+        //cleanup
+        File.Delete(totalPath);
+        Assert.That(File.Exists(totalPath), Is.False);
+    }
+    
+    // TODO check of content of file
+    [Test]
+    public async Task StoreDiff_called_rightContentInFile()
+    {
+        //act
+        List<DiffData> data = DiffComputer.CreateDiff(Path.Combine(_projectRoot, "test.csv"));
+        bool result = await DiffStore.StoreDiffToCsv(data, _projectRoot, "test_diff");
+        
+        var totalPath = Path.Join(_projectRoot, "test_diff.csv");
+        using var reader = new StreamReader(totalPath);
+        //TODO read lines and check content
+        reader.ReadLine();
+        //assert
+        
+        //cleanup
+        File.Delete(totalPath);
+        Assert.That(File.Exists(totalPath), Is.False);
     }
 }
