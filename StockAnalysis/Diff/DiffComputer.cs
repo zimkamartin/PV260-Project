3f7bdaa1779ffd2a51b8dfc0982adfb5a33f754a
Zdenka Valkučáková <xvalkuc@fi.muni.cz>
2024-03-24 10:16:41 +0100

    fix: file decomposition of DiffComputer

--- StockAnalysis/Diff/DiffComputer.cs
+++ StockAnalysis/Diff/DiffComputer.cs
@@ -1,34 +1,14 @@
 ﻿using CsvHelper;
-using CsvHelper.Configuration.Attributes;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
 namespace StockAnalysis.Diff;
 
-// FIXME: Move to separate file. File name = class name
-public class FundData
-{
-    [Name("date")] public DateTime Date { get; set; }
-
-    [Name("fund")] public string Fund { get; set; }
-
-    [Name("company")] public string Company { get; set; }
-
-    [Name("ticker")] public string Ticker { get; set; }
-
-    [Name("cusip")] public string Cusip { get; set; }
-
-    [Name("shares")] public string Shares { get; set; }
-
-    [Name("market value ($)")] public string MarketValue { get; set; }
-
-    [Name("weight (%)")] public string Weight { get; set; }
-}
-
 public static class DiffComputer
 {
     public static List<DiffData> CreateDiff(string path)
     {
+        
         // FIXME: constants "-old" and "-new"
         var oldFile = Path.GetDirectoryName(path) + Path.DirectorySeparatorChar +
                       Path.GetFileNameWithoutExtension(path) + "-old" + Path.GetExtension(path);
@@ -100,12 +80,3 @@ public static class DiffComputer
     }
 }
 
-public class DiffData
-{
-    public string Company { get; set; }
-    public string Ticker { get; set; }
-    public double SharesChange { get; set; }
-    public double MarketValueChange { get; set; }
-    public double Weight { get; set; }
-    public bool NewEntry { get; set; }
-}
