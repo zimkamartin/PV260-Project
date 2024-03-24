a65cba14aa50c3e80e570f9f9357a020c3864ad5
Martin Mojžiš <xmojzis1@fi.muni.cz>
2024-03-24 09:32:54 +0100

    fix bad merge conflict resolve

--- StockAnalysis/Constants/Constants.cs
+++ StockAnalysis/Constants/Constants.cs
@@ -3,8 +3,6 @@ namespace StockAnalysis.Constants;
 public static class Constants
 {
     public const string CsvExtension = ".csv";
-    public const string NewSuffix = "-new";
-    public const string OldSuffix = "-old";
     public const string CsvSeparator = ";";
     public const string CsvSharesUpIndicator = " up%";
     public const string CsvSharesDownIndicator = " down%";
