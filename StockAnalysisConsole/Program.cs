80189c59b62bf917c55797e03f5fd998286df24d
Matej Vavrek <matvav13@gmail.com>
2024-03-25 16:10:35 +0100

    fix: console app demo refactors

--- StockAnalysisConsole/Program.cs
+++ StockAnalysisConsole/Program.cs
@@ -1,4 +1,8 @@
 using StockAnalysis.Constants;
+using StockAnalysis.Diff.Compute;
+using StockAnalysis.Diff.Load;
+using StockAnalysis.Diff.Store;
+using StockAnalysis.Download;
 using StockAnalysis.Download.Getter;
 using StockAnalysis.Download.PeriodicalDownload;
 using StockAnalysis.Download.Store;
@@ -36,15 +40,17 @@ namespace StockAnalysisConsole
                     SenderMail,
                     true,
                     ClientHost));
-
-            var manager = new AnalysisManager(new CsvDownload(),
-                new CsvStorage()
-            );
-
-            // Create client for download.
+            
             using var client = new HttpClient();
             client.DefaultRequestHeaders.Add("User-Agent", "Other");
+
+            var downloadManager =
+                new DownloadManager(Paths.GetDownloadFolderPath(), new CsvDownload(), 
+                                new CsvStorage(), client);
             
+            var manager = new AnalysisManager(downloadManager, new CsvDiffComputer(new CsvHoldingLoader()), 
+                                                new CsvDiffStore());
+
             List<string> diffPaths;
             try
             {
@@ -119,16 +125,17 @@ namespace StockAnalysisConsole
         /// <summary>
         /// Sends e-mails to recipients.
         /// </summary>
-        private static async Task SendEmails(Sender sender, string[] addresses, List<string> diffPaths)
+        private static async Task SendEmails(ISender sender, IEnumerable<string> addresses, IEnumerable<string> diffPaths)
         {
             try
             {
                 Console.WriteLine("Analysis completed. Sending e-mails... ");
                 await sender.SendNotification(addresses, diffPaths);
             }
-            catch (Exception)
+            catch (SenderException)
             {
                 Console.WriteLine("Email sending failed.");
+                return;
             }
 
             Console.WriteLine("E-mails sent. Check your inbox.");
