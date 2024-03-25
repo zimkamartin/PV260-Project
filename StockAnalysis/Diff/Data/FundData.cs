594e99acad748720ce28ea55efb49ffd06abeb0a
Matej Vavrek <matvav13@gmail.com>
2024-03-25 20:45:44 +0100

    fix: supressed warnings.

--- StockAnalysis/Diff/Data/FundData.cs
+++ StockAnalysis/Diff/Data/FundData.cs
@@ -6,17 +6,17 @@ public class FundData
 {
     [Name("date")] public DateTime Date { get; set; }
 
-    [Name("fund")] public string Fund { get; set; }
+    [Name("fund")] public string Fund { get; set; } = null!;
 
-    [Name("company")] public string Company { get; set; }
+    [Name("company")] public string Company { get; set; } = null!;
 
-    [Name("ticker")] public string Ticker { get; set; }
+    [Name("ticker")] public string Ticker { get; set; } = null!;
 
-    [Name("cusip")] public string Cusip { get; set; }
+    [Name("cusip")] public string Cusip { get; set; } = null!;
 
-    [Name("shares")] public string Shares { get; set; }
+    [Name("shares")] public string Shares { get; set; } = null!;
 
-    [Name("market value ($)")] public string MarketValue { get; set; }
+    [Name("market value ($)")] public string MarketValue { get; set; } = null!;
 
-    [Name("weight (%)")] public string Weight { get; set; }
+    [Name("weight (%)")] public string Weight { get; set; } = null!;
 }
