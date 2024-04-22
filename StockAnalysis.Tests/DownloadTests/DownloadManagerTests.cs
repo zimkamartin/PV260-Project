c86ecae6d54a98eef4f231afcf9a126c8dc56e0a
Matej Vavrek <matvav13@gmail.com>
2024-04-22 16:54:54 +0200

    fix: DownloadManager tests should no longer be downloading from an external API.

--- StockAnalysis.Tests/DownloadTests/DownloadManagerTests.cs
+++ StockAnalysis.Tests/DownloadTests/DownloadManagerTests.cs
@@ -2,6 +2,9 @@ using StockAnalysis.Download.Getter;
 using StockAnalysis.Download.Manager;
 using StockAnalysis.Download.Store;
 using StockAnalysis.HoldingsConfig;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
 
 namespace StockAnalysisTests.DownloadTests;
 
@@ -9,7 +12,45 @@ public class DownloadManagerTests
 {
     private const string StoragePath = ".";
     private const string StorageDir = "download-test";
+    private WireMockServer server;
+    
+    [SetUp]
+    public void Setup()
+    {
+        server = WireMockServer.Start(9876);
+        // Most of the body is simply arbitrary data and has no effect on tests.
+        server.Given(Request.Create().WithPath("/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv").UsingGet()
+        ).RespondWith(
+            Response
+                    .Create()
+                    .WithStatusCode(200)
+                    .WithHeader("Content-Type", "text/csv")
+                    .WithBody("date,fund,company,ticker,cusip,shares,\"market value ($)\",\"weight (%)\"\n" +
+                              "04/16/2024,ARKK,\"TESLA INC\",TSLA,88160R101,\"4,028,071\",\"$650,452,905.08\",9.83%\n" +
+                              "04/16/2024,ARKK,\"COINBASE GLOBAL INC -CLASS A\",COIN,19260Q107,\"2,630,233\",\"$587,620,354.53\",8.88%\n" +
+                              "04/16/2024,ARKK,\"ROKU INC\",ROKU,77543R102,\"8,941,303\",\"$527,000,398.82\",7.96%\n" +
+                              "04/16/2024,ARKK,\"BLOCK INC\",SQ,852234103,\"6,171,325\",\"$453,592,387.50\",6.85%\n")
+            );
+        server.Given(Request.Create().WithPath("/ARK_GENOMIC_REVOLUTION_ETF_ARKG_HOLDINGS.csv").UsingGet()
+        ).RespondWith(
+            Response
+                .Create()
+                .WithStatusCode(200)
+                .WithHeader("Content-Type", "text/csv")
+                .WithBody("date,fund,company,ticker,cusip,shares,\"market value ($)\",\"weight (%)\"\n" +
+                          "04/16/2024,ARKG,\"TESLA INC\",TSLA,88160R101,\"4,028,071\",\"$650,452,905.08\",9.83%\n" +
+                          "04/16/2024,ARKG,\"COINBASE GLOBAL INC -CLASS A\",COIN,19260Q107,\"2,630,233\",\"$587,620,354.53\",8.88%\n" +
+                          "04/16/2024,ARKG,\"ROKU INC\",ROKU,77543R102,\"8,941,303\",\"$527,000,398.82\",7.96%\n" +
+                          "04/16/2024,ARKG,\"BLOCK INC\",SQ,852234103,\"6,171,325\",\"$453,592,387.50\",6.85%\n")
+        );
+    }
 
+    [TearDown]
+    public void Teardown()
+    {
+        server.Stop();
+    }
+    
     [Test]
     public async Task GetHoldings_ValidUriSingleCsv_Succeeds()
     {
@@ -17,7 +58,7 @@ public class DownloadManagerTests
         HoldingInformation[] holdings =
         {
             new("ARKK-Holdings",
-                "https://ark-funds.com/wp-content/uploads/funds-etf-csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv")
+                "http://localhost:9876/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv")
         };
         var resultName = holdings[0].Name + ".csv";
         var resultDir = Path.Combine(StoragePath, StorageDir);
@@ -51,9 +92,9 @@ public class DownloadManagerTests
         HoldingInformation[] holdings =
         {
             new("ARKK-Holdings",
-                "https://ark-funds.com/wp-content/uploads/funds-etf-csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv"),
+                "http://localhost:9876/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv"),
             new("ARKG-Holdings",
-                "https://ark-funds.com/wp-content/uploads/funds-etf-csv/ARK_GENOMIC_REVOLUTION_ETF_ARKG_HOLDINGS.csv")
+                "http://localhost:9876/ARK_GENOMIC_REVOLUTION_ETF_ARKG_HOLDINGS.csv")
         };
         var resultDir = Path.Combine(StoragePath, StorageDir);
         using var client = new HttpClient();
